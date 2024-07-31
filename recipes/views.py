08154a289cd2beda900eff380834c3b431652ad8
Renato Oliveira <renato.j.olveira@gmail.com>
2024-07-31 19:57:18 -0300

    Removid sobre and contato

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def home(request):
     return render (request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('WELCOME TO CONTATO 📱')
-
-def sobre(request):
-    return HttpResponse('WELCOME TO SOBRE 🗃️')
